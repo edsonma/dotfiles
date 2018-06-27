e95ba7cbd5377ebb666589f17b0791f55a6be676
Edson Lek Hong Ma <edsonma@gmail.com>
2018-06-27 01:14:48 -0300

    fix for better dotfiles

---
+++ link_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author: Edson Ma
+# Put everything in the right place
+
+# Symbolic links
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/system/alias ~/.alias
+ln -s ~/dotfiles/system/zshrc ~/.zshrc
+ln -s ~/dotfiles/system/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/git/gitconfig ~/.gitconfig
+
+
+
