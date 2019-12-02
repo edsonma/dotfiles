62e10cbcbe2010bad788d2a2c29be971a4fbb89c
Edson Ma <edsonma@gmail.com>
2019-12-02 00:24:22 -0300

    using asdf as ruby selector

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -31,11 +31,11 @@ create_symbolic_links() {
   ln -s ~/dotfiles/vim/vimrc ~/.vimrc
   ln -s ~/dotfiles/vim ~/.vim
   ln -s ~/dotfiles/system/alias ~/.alias
+  ln -s ~/dotfiles/system/asdfrc ~/.asdfrc
   ln -s ~/dotfiles/system/zshrc ~/.zshrc
   ln -s ~/dotfiles/system/oh-my-zsh ~/.oh-my-zsh
   ln -s ~/dotfiles/tools/asdf ~/.asdf
   ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
-
   if [[ $platform == 'Linux' ]]; then
     ln -s ~/dotfiles/system/linuxbrew ~/.linuxbrew
   fi
