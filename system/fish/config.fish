094892072f37679915e4426a5103c66cbf2756d8
edsonma@gmail.com <edsonma@gmail.com>
2025-02-03 18:29:07 -0300

    chore: remove not used gcloud sdk

--- system/fish/config.fish
+++ system/fish/config.fish
@@ -4,9 +4,6 @@ status --is-interactive; and ~/.rbenv/bin/rbenv init - fish | source
 # startship
 starship init fish | source
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/edsonma/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/edsonma/Downloads/google-cloud-sdk/path.fish.inc'; end
-
 set -x JAVA_HOME (/usr/libexec/java_home  -v 20.0.1)
 
 fish_add_path /opt/homebrew/bin
