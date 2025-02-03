a23c3ed90de8f7bc2ce23960f2b4f51d3b74622f
edsonma@gmail.com <edsonma@gmail.com>
2025-02-03 16:06:03 -0300

    chore: updates

--- system/fish/config.fish
+++ system/fish/config.fish
@@ -10,3 +10,11 @@ if [ -f '/Users/edsonma/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/e
 set -x JAVA_HOME (/usr/libexec/java_home  -v 20.0.1)
 
 fish_add_path /opt/homebrew/bin
+
+function nvm
+  bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
+end
+
+set -x NVM_DIR ~/.nvm
+nvm use default --silent
+
